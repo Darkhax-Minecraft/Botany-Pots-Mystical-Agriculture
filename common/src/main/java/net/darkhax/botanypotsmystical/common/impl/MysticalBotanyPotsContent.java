6d351202cc19cd053e195b354df360d11c26c95d
Tyler Hancock <darklime@live.ca>
2025-10-16 17:50:36 -0600

    Update build dependencies.

--- common/src/main/java/net/darkhax/botanypotsmystical/common/impl/MysticalBotanyPotsContent.java
+++ common/src/main/java/net/darkhax/botanypotsmystical/common/impl/MysticalBotanyPotsContent.java
@@ -1,19 +1,19 @@
 package net.darkhax.botanypotsmystical.common.impl;
 
-import net.darkhax.bookshelf.common.api.registry.IContentProvider;
-import net.darkhax.bookshelf.common.api.registry.register.Register;
+import net.darkhax.bookshelf.common.api.registry.ContentProvider;
+import net.darkhax.bookshelf.common.api.registry.adapters.GameRegistryAdapter;
 import net.darkhax.botanypotsmystical.common.impl.data.recipe.crop.MysticalCrop;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
-public class MysticalBotanyPotsContent implements IContentProvider {
+public class MysticalBotanyPotsContent implements ContentProvider {
 
     @Override
-    public String contentNamespace() {
+    public String namespace() {
         return BotanyPotsMysticalMod.MOD_ID;
     }
 
     @Override
-    public void registerRecipeSerializers(Register<RecipeSerializer<?>> registry) {
+    public void defineRecipeSerializers(GameRegistryAdapter<RecipeSerializer<?>> registry) {
         registry.add("mystical_crop", MysticalCrop.SERIALIZER);
     }
 }
